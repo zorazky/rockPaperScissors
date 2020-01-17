d28cde298a0f58d7cdc5a0810951e29fdf6017da
zorazky <oswaldo.vazquez@gmail.com>
2020-01-17 11:14:04 -0600

    functions as arguments

--- app.js
+++ app.js
@@ -77,3 +77,22 @@ startGameBtn.addEventListener('click', () => {
     alert(message);
     gameIsRunning = false;
 });
+
+const sumUp = (resultHandler, ...numbers) => {
+    const validateNumber = (number) => {
+        return isNaN(number) ? 0 : number;
+    }
+    let sum = 0;
+    for (const num of numbers) {
+        sum += validateNumber(num);
+    }
+
+    resultHandler(sum);
+}
+
+const showResult = (result) => {
+    alert('The result after adding all numbers is: ' + result);
+}
+
+
+sumUp(showResult, 1, 2, 3, 4);
